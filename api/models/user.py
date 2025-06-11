e14e691be8a99f47cef04b59f1b3f46ef2b19a15
Arian-Ott <88984301+Arian-Ott@users.noreply.github.com>
2025-06-11 12:49:08 +0200

    feat: add UserProfile model for optional user settings; remove EmailTokenModel and EmailTokenReason classes
    Signed-off-by: Arian-Ott <88984301+Arian-Ott@users.noreply.github.com>

--- api/models/user.py
+++ api/models/user.py
@@ -7,6 +7,8 @@ import uuid
 class UserModel(Base):
     """
     User model for the application.
+    Those are the minimum fields required for the user to be created.
+    Since storify does not require unnecessary data, email and other fields are considered optional and thus are handled in a different table to reduce overhead.
     """
 
     __tablename__ = "users"
@@ -14,7 +16,8 @@ class UserModel(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String(50), unique=True, nullable=False, index=True)
     password = Column(String(128), nullable=False)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean, default=True, info={"description": "Indicates if the user account is active. Has nothing to do with the email verification of api/models/settings.py"})
     created_at = Column(DateTime, default=datetime.now)
     last_password_change = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
